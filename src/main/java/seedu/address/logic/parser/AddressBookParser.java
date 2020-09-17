8fe1129e8257e62d4581f3dbd0123be81984458e
lyeyixian <lyeyx999@hotmail.com>
2020-09-17 15:20:30 +0800

    Fix code that violates checkstyle test

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -59,10 +59,10 @@ public class AddressBookParser {
 
         case FindCommand.COMMAND_WORD:
             return new FindCommandParser().parse(arguments);
-            
+
         case RemarkCommand.COMMAND_WORD:
             return new RemarkCommandParser().parse(arguments);
-            
+
         case ListCommand.COMMAND_WORD:
             return new ListCommand();
 
