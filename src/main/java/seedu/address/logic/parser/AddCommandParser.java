35f4be16446afedc1a4fd2f0acda2e9d7fdf206a
LAPTOP-2DEMJIFC\haish <haishanlim@gmail.com>
2020-09-23 21:17:25 +0800

    Update with checkstylemain

--- src/main/java/seedu/address/logic/parser/AddCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddCommandParser.java
@@ -12,13 +12,14 @@ import java.util.stream.Stream;
 
 import seedu.address.logic.commands.AddCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.person.Address;
+import seedu.address.model.person.Email;
+import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
 import seedu.address.model.person.Phone;
-import seedu.address.model.person.Name;
-import seedu.address.model.person.Email;
-import seedu.address.model.person.Address;
-import seedu.address.model.tag.Tag;
 import seedu.address.model.person.Remark;
+import seedu.address.model.tag.Tag;
+
 
 
 /**
