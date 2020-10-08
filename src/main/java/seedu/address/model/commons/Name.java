64812d22c337de53a6948f22e5bfb111c62779b0
jiaweiteo <jiawei.teo@hotmail.com>
2020-10-08 18:21:33 +0800

    Resolve Test Cases

--- src/main/java/seedu/address/model/commons/Name.java
+++ src/main/java/seedu/address/model/commons/Name.java
@@ -16,7 +16,7 @@ public class Name {
      * The first character of the Name must not be a whitespace,
      * otherwise " " (a blank string) becomes a valid input.
      */
-    public static final String VALIDATION_REGEX = "(\\p{Graph}+\\p{Space}?)+";
+    public static final String VALIDATION_REGEX = "[\\p{Alnum}][\\p{Alnum} ]*";
 
     public final String name;
 
