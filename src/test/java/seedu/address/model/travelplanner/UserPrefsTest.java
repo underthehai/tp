04e6982e9d4defc965c9e6bc9c2213f6a1a59729
lyeyixian <lyeyx999@hotmail.com>
2020-10-11 22:00:45 +0800

    Add UserPrefsTest class

---
+++ src/test/java/seedu/address/model/travelplanner/UserPrefsTest.java
@@ -0,0 +1,19 @@
+package seedu.address.model.travelplanner;
+
+import org.junit.jupiter.api.Test;
+import static seedu.address.testutil.Assert.assertThrows;
+
+public class UserPrefsTest {
+
+    @Test
+    public void setGuiSettings_nullGuiSettings_throwsNullPointerException() {
+        UserPrefs userPref = new UserPrefs();
+        assertThrows(NullPointerException.class, () -> userPref.setGuiSettings(null));
+    }
+
+    @Test
+    public void setTravelPlannerFilePath_nullPath_throwsNullPointerException() {
+        UserPrefs userPrefs = new UserPrefs();
+        assertThrows(NullPointerException.class, () -> userPrefs.setTravelPlannerFilePath(null));
+    }
+}
