3508238b5fe726f262dd9f70afcf75007724060b
Jeanne Toh <jeannetoh99@gmail.com>
2020-11-01 14:58:26 +0800

    Handle Duplicate Error in Json file on initialising app

--- src/main/java/seedu/address/model/travelplan/exceptions/DuplicateTravelPlanException.java
+++ src/main/java/seedu/address/model/travelplan/exceptions/DuplicateTravelPlanException.java
@@ -1,11 +1,13 @@
 package seedu.address.model.travelplan.exceptions;
 
+import seedu.address.commons.exceptions.DuplicateException;
+
 /**
  * Signals that the operation will result in duplicate TravelPlans (TravelPlans are considered duplicates if they have
  * the same
  * identity).
  */
-public class DuplicateTravelPlanException extends RuntimeException {
+public class DuplicateTravelPlanException extends DuplicateException {
     public DuplicateTravelPlanException() {
         super("Operation would result in duplicate persons");
     }
