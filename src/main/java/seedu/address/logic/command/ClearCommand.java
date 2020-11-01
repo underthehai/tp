83ab23003c874ec67af701c732cefdaad148ff54
jiaweiteo <jiawei.teo@hotmail.com>
2020-11-01 16:24:18 +0800

    no message

--- src/main/java/seedu/address/logic/command/ClearCommand.java
+++ src/main/java/seedu/address/logic/command/ClearCommand.java
@@ -3,6 +3,7 @@ package seedu.address.logic.command;
 import static java.util.Objects.requireNonNull;
 
 import seedu.address.model.Model;
+import seedu.address.model.TravelPlanner;
 import seedu.address.model.commons.Name;
 import seedu.address.model.commons.WanderlustDate;
 import seedu.address.model.travelplan.TravelPlan;
@@ -19,8 +20,7 @@ public class ClearCommand extends Command {
     @Override
     public CommandResult execute(Model model) {
         requireNonNull(model);
-        model.setTravelPlan(new TravelPlan(new Name("Dummy"), new WanderlustDate("0"), new WanderlustDate("0")),
-                new TravelPlan(null, null, null));
+        model.setTravelPlanner(new TravelPlanner(null));
         return new CommandResult(MESSAGE_SUCCESS);
     }
 }
