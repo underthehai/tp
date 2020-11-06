c04d03c9a01192829e71584adeb49925cdfeda0c
jiaweiteo <jiawei.teo@hotmail.com>
2020-11-06 18:45:28 +0800

    Sort Command Review

---
+++ src/test/java/seedu/address/logic/command/sort/SortActivityCommandTest.java
@@ -0,0 +1,129 @@
+package seedu.address.logic.command.sort;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import seedu.address.model.Model;
+import seedu.address.model.ModelManager;
+import seedu.address.model.UserPrefs;
+import seedu.address.model.activity.Activity;
+
+import java.util.Comparator;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static seedu.address.logic.command.CommandTestUtil.*;
+import static seedu.address.logic.parser.ParserUtil.ACTIVITY_INDEX;
+import static seedu.address.testutil.typicals.TypicalTravelPlans.getTypicalTravelPlanner;
+
+public class SortActivityCommandTest {
+
+    public static final String KEYWORD_NAME = "name";
+    public static final String KEYWORD_COST = "cost";
+    public static final String KEYWORD_IMPORTANCE = "importance";
+    public static final String KEYWORD_DATETIME = "datetime";
+
+    private Model model;
+
+    @BeforeEach
+    public void setUp() {
+        model = new ModelManager(getTypicalTravelPlanner(), new UserPrefs());
+        model.setDirectory(0);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        model = null;
+    }
+
+    @Test
+    public void execute_sortActivityName_success() {
+        SortActivityCommand sortActivityCommand = new SortActivityCommand(KEYWORD_NAME);
+
+        String expectedMessage = String.format(SortActivityCommand.MESSAGE_SORT_ACTIVITY_SUCCESS,
+                KEYWORD_NAME);
+
+        ModelManager expectedModel = new ModelManager(getTypicalTravelPlanner(), new UserPrefs());
+
+        Comparator<Activity> nameComparator = Comparator.comparing(d -> d.getName().toString());
+
+        expectedModel.setDirectory(0);
+        expectedModel.sortWishlist(nameComparator);
+
+        assertCommandSuccess(sortActivityCommand, model, expectedMessage, expectedModel, ACTIVITY_INDEX);
+    }
+
+    @Test
+    public void execute_sortActivityCost_success() {
+        SortActivityCommand sortActivityCommand = new SortActivityCommand(KEYWORD_COST);
+
+        String expectedMessage = String.format(SortActivityCommand.MESSAGE_SORT_ACTIVITY_SUCCESS,
+                KEYWORD_COST);
+
+        ModelManager expectedModel = new ModelManager(getTypicalTravelPlanner(), new UserPrefs());
+
+        Comparator<Activity> costComparator = Comparator.comparingInt(c -> -c.getCostAsInt());
+
+        expectedModel.setDirectory(0);
+        expectedModel.sortWishlist(costComparator);
+
+        assertCommandSuccess(sortActivityCommand, model, expectedMessage, expectedModel, ACTIVITY_INDEX);
+    }
+
+    @Test
+    public void execute_sortActivityImportance_success() {
+        SortActivityCommand sortActivityCommand = new SortActivityCommand(KEYWORD_IMPORTANCE);
+
+        String expectedMessage = String.format(SortActivityCommand.MESSAGE_SORT_ACTIVITY_SUCCESS,
+                KEYWORD_IMPORTANCE);
+
+        ModelManager expectedModel = new ModelManager(getTypicalTravelPlanner(), new UserPrefs());
+
+        Comparator<Activity> importanceComparator = Comparator.comparing(i -> -i.getImportanceAsInt());
+
+        expectedModel.setDirectory(0);
+        expectedModel.sortWishlist(importanceComparator);
+
+        assertCommandSuccess(sortActivityCommand, model, expectedMessage, expectedModel, ACTIVITY_INDEX);
+    }
+
+    @Test
+    public void execute_sortActivityDateTime_success() {
+        SortActivityCommand sortActivityCommand = new SortActivityCommand(KEYWORD_DATETIME);
+
+        String expectedMessage = String.format(SortActivityCommand.MESSAGE_SORT_ACTIVITY_SUCCESS,
+                KEYWORD_DATETIME);
+
+        ModelManager expectedModel = new ModelManager(getTypicalTravelPlanner(), new UserPrefs());
+
+        Comparator<Activity> dateComparator = Comparator.comparing(d -> d.getActivityDateTime().getValue());
+
+        expectedModel.setDirectory(0);
+        expectedModel.sortWishlist(dateComparator);
+
+        assertCommandSuccess(sortActivityCommand, model, expectedMessage, expectedModel, ACTIVITY_INDEX);
+    }
+
+    @Test
+    public void equals() {
+        SortActivityCommand sortActivityFirstCommand = new SortActivityCommand(KEYWORD_NAME);
+        SortActivityCommand sortActivitySecondCommand = new SortActivityCommand(KEYWORD_COST);
+
+        // same object -> returns true
+        assertTrue(sortActivityFirstCommand.equals(sortActivityFirstCommand));
+
+        // same values -> returns true
+        SortActivityCommand sortActivityFirstCommandCopy = new SortActivityCommand(KEYWORD_NAME);
+        assertTrue(sortActivityFirstCommand.equals(sortActivityFirstCommandCopy));
+
+        // different types -> returns false
+        assertFalse(sortActivityFirstCommand.equals(1));
+
+        // null -> returns false
+        assertFalse(sortActivityFirstCommand.equals(null));
+
+        // different sort keyword -> returns false
+        assertFalse(sortActivityFirstCommand.equals(sortActivitySecondCommand));
+    }
+
+}
--- src/test/java/seedu/address/logic/parser/CommandParserTestUtil.java
+++ src/test/java/seedu/address/logic/parser/CommandParserTestUtil.java
@@ -6,6 +6,7 @@ import seedu.address.logic.command.Command;
 import seedu.address.logic.command.FindCommand;
 import seedu.address.logic.command.delete.DeleteActivityCommand;
 import seedu.address.logic.command.edit.EditCommand;
+import seedu.address.logic.command.sort.SortCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
 
 /**
@@ -28,20 +29,6 @@ public class CommandParserTestUtil {
     }
 
     /**
-     * Asserts that the DeleteCommand parsing of {@code userInput} by {@code parser} is successful
-     * and the command created equals to {@code expectedCommand}.
-     */
-    public static void assertWanderLustDeleteParseSuccess(DeleteCommandParser parser, String userInput,
-                                                          DeleteActivityCommand expectedCommand) {
-        try {
-            seedu.address.logic.command.Command command = parser.parse(userInput);
-            assertEquals(expectedCommand, command);
-        } catch (seedu.address.logic.parser.exceptions.ParseException pe) {
-            throw new IllegalArgumentException("Invalid userInput.", pe);
-        }
-    }
-
-    /**
      * Asserts that the FindCommand parsing of {@code userInput} by {@code parser} is successful and the command created
      * equals to {@code expectedCommand}.
      */
@@ -56,12 +43,47 @@ public class CommandParserTestUtil {
     }
 
     /**
+     * Asserts that the FindCommand parsing of {@code userInput} by {@code parser} is successful and the command created
+     * equals to {@code expectedCommand}.
+     */
+    public static void assertSortParserSuccess(SortCommandParser parser, String userInput,
+                                               SortCommand expectedCommand) {
+        try {
+            Command command = parser.parse(userInput);
+            assertEquals(expectedCommand, command);
+        } catch (ParseException pe) {
+            throw new IllegalArgumentException("Invalid userInput.", pe);
+        }
+    }
+
+    /**
      * Asserts that the parsing of {@code userInput} by {@code parser} is unsuccessful and the error message
      * equals to {@code expectedMessage}.
-     @@ -35,4 +51,18 @@ public static void assertParseFailure(WanderlustParser parser, String userInput, String ex
-     assertEquals(expectedMessage, pe.getMessage());
-     }
-     }
+     */
+    public static void assertSortParserFailure(SortCommandParser parser, String userInput,
+                                                    String expectedMessage) {
+        try {
+            parser.parse(userInput);
+            throw new AssertionError("The expected ParseException was not thrown.");
+        } catch (seedu.address.logic.parser.exceptions.ParseException pe) {
+            assertEquals(expectedMessage, pe.getMessage());
+        }
+    }
+
+
+    /**
+     * Asserts that the DeleteCommand parsing of {@code userInput} by {@code parser} is successful
+     * and the command created equals to {@code expectedCommand}.
+     */
+    public static void assertWanderLustDeleteParseSuccess(DeleteCommandParser parser, String userInput,
+                                                          DeleteActivityCommand expectedCommand) {
+        try {
+            Command command = parser.parse(userInput);
+            assertEquals(expectedCommand, command);
+        } catch (seedu.address.logic.parser.exceptions.ParseException pe) {
+            throw new IllegalArgumentException("Invalid userInput.", pe);
+        }
+    }
 
      /**
       * Asserts that the parsing of {@code userInput} by {@code parser} is unsuccessful and the error message
